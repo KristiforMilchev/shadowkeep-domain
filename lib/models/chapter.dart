d648b12b23608743ac31f5b78fdd9bca89091e47
Kristifor Milchev <km@airzen.io>
2023-06-16 20:16:50 +0300

    Extending the relation binding model to hold realtions as inner objects

--- lib/models/chapter.dart
+++ lib/models/chapter.dart
@@ -6,11 +6,15 @@ class Chapter {
   int bookId;
   List<Page> pages;
   int? relatedTo;
+  String? shortDescription;
+
   Chapter({
     required this.id,
     required this.bookId,
     required this.pageTitle,
     required this.pages,
     this.relatedTo,
+    this.shortDescription =
+        "here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
   });
 }
--- lib/models/relational_binding.dart
+++ lib/models/relational_binding.dart
@@ -5,10 +5,12 @@ class RelationalBinding {
   int id;
   Widget content;
   int? relatedTo;
+  List<RelationalBinding> related;
 
   RelationalBinding({
     required this.id,
     required this.content,
+    this.related = const [],
     this.relatedTo,
   });
 }
