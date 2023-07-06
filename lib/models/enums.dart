bf6c66c60c1a6bc7fdacf27ffaf4dc6695b95e13
Kristifor Milchev <km@airzen.io>
2023-07-06 18:46:00 +0300

    Adding new enum for the editor commands

--- lib/models/enums.dart
+++ lib/models/enums.dart
@@ -5,3 +5,21 @@ enum PageTransition { easeInAndOut, slideForward, slideBack }
 enum AuthFlow { login, register }
 
 enum Day { monday, tuesday, wednesday, thursday, friday, saturday, sunday }
+
+enum EditorCommand {
+  undo,
+  redo,
+  copy,
+  paste,
+  alightLeft,
+  alightRight,
+  alignCenter,
+  bulletList,
+  orderedList,
+  wrapDoubleQuoute,
+  wrapSingleQuoute,
+  selectAll,
+  delete,
+  bold,
+  underline
+}
